20aebc0006a4fe94be568a3068fb892ac8324c79
TANGIRALA JAYA SREE LAKSHMI SAVITRI <tjayasreels.cs19@rvce.edu.in>
2020-08-16 10:52:14 +0530

    My 4th Assignment in Coursera

---
+++ coursera/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function(window) {
+	var byeSpeaker = function (name) {
+  		console.log("Good Bye " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
+
+})(window);
---
+++ coursera/SpeakHello.js
@@ -0,0 +1,8 @@
+(function(window) {
+	var helloSpeaker = function (name) {
+		console.log("Hello " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
